02e160a86d5867681ea4645e1d20efeeaf669fd2
Daniel-22-16 <1475937@senati.pe>
2023-03-17 07:41:30 -0500

    java4

--- Daniel.java
+++ Daniel.java
@@ -1,17 +1,28 @@
 
 package daniel;
+import java.util.Scanner;
 
 public class Daniel {
 
     public static void main(String[] args) 
     {
-     System.out.println("hola daniel");
-     int numero = 8;
-     
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre =sc.nextLine();
+        System.out.println("hola " +nombre);
+        System.out.println("porfavor ingrese un numero:");
+        
+        int numero = sc.nextInt();
+        
+        
      if(numero<=10){
     System.out.println("el numero ingresado es menor que 10");
-    }    
+}else{
+        System.out.println("el numero ingresado es mayor que 10");
+        
+     }
     
-    }
+}
 }
 
+    
